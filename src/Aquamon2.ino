83a1da17f9967fb8c3dcddfbc672766864e97256
Stephen Hill <citizenhill@gmail.com>
2016-11-03 19:18:32 -0230

    Tidying up code.
    Folding and commenting sections. Need to work on radiopacket string
    next.

--- src/Aquamon2.ino
+++ src/Aquamon2.ino
@@ -1,15 +1,18 @@
-#include <Arduino.h>
+//Libraries
+  #include <Arduino.h>
+  #include <OneWire.h>  //OneWire
 
-/* RFM69 library and code by Felix Rusu - felix@lowpowerlab.com
-Aquamon
-// **********************************************************************************/
+  /* RFM69 library and code by Felix Rusu - felix@lowpowerlab.com
+  Aquamon
+  // **********************************************************************************/
 
-#include <RFM69.h>    //get it here: https://www.github.com/lowpowerlab/rfm69
-#include <SPI.h>
-#include <OneWire.h>
-#include <stdio.h>
-#include <avr/dtostrf.h>
+  #include <RFM69.h>    //get it here: https://www.github.com/lowpowerlab/rfm69
+  #include <SPI.h>
+  #include <stdio.h>
+  #include <avr/dtostrf.h>
 
+
+//Global variables anc constants
 //*********************************************************************************************
 // *********** IMPORTANT SETTINGS - YOU MUST CHANGE/ONFIGURE TO FIT YOUR HARDWARE *************
 //*********************************************************************************************
@@ -31,7 +34,7 @@ Aquamon
 #define RFM69_IRQN    3  // Pin 3 is IRQ 3!
 #define RFM69_RST     4
 
-/*Feather m0 w/wing 
+/*Feather m0 w/wing
 #define RFM69_RST     11   // "A"
 #define RFM69_CS      10   // "B"
 #define RFM69_IRQ     6    // "D"
@@ -39,18 +42,22 @@ Aquamon
 */
 
 #define LED           13  // onboard blinky
+int numofsensors = 2 // The number of sensors which will send results to the radiopacket.
 
 int16_t packetnum = 0;  // packet counter, we increment per xmission
 
 RFM69 radio = RFM69(RFM69_CS, RFM69_IRQ, IS_RFM69HCW, RFM69_IRQN);
 OneWire  ds(10);  // on pin 10 (a 4.7K resistor is necessary)
 
-void setup() {
-//  while (!Serial); // wait until serial console is open, remove if not tethered to computer. Delete this line on ESP8266
+
+
+void setup() { // Setup, mostly radio stuff
+
+  //  while (!Serial); // wait until serial console is open, remove if not tethered to computer. Delete this line on ESP8266
   Serial.begin(SERIAL_BAUD);
 
   Serial.println("Feather RFM69HCW Transmitter");
-  
+
   // Hard Reset the RFM module
   pinMode(RFM69_RST, OUTPUT);
   digitalWrite(RFM69_RST, HIGH);
@@ -64,9 +71,9 @@ void setup() {
     radio.setHighPower();    // Only for RFM69HCW & HW!
   }
   radio.setPowerLevel(31); // power output ranges from 0 (5dBm) to 31 (20dBm)
-  
+
   radio.encrypt(ENCRYPTKEY);
-  
+
   pinMode(LED, OUTPUT);
   Serial.print("\nTransmitting at ");
   Serial.print(FREQUENCY==RF69_433MHZ ? 433 : FREQUENCY==RF69_868MHZ ? 868 : 915);
@@ -74,18 +81,18 @@ void setup() {
 
 }
 
-
 void loop() {
 
-// Temperature reads
- byte i;
+// TEMPERATURE READS ON OneWire
+  // Temperature variables.
+  byte i;
   byte present = 0;
   byte type_s;
   byte data[12];
   byte addr[8];
   float celsius, fahrenheit;
-  
 
+  // Checks for the presence of OneWire probes and returns if none found.
   if ( !ds.search(addr)) {
     Serial.println("No more addresses.");
     Serial.println();
@@ -93,22 +100,23 @@ void loop() {
     delay(250); //Delay in cycles between temperature reads.
     return;
   }
-  
+
   Serial.print("ROM =");
   String ROM ="ROM";
   for( i = 0; i < 8; i++) {
     Serial.write(' ');
     Serial.print(addr[i]); //Can add ,HEX if want to represent as HEX
-    ROM = ROM+(addr[i]);
+    ROM = ROM+(addr[i]); //Assembles ROM string.
   }
 
-  
+  //No idea what this does!
   if (OneWire::crc8(addr, 7) != addr[7]) {
       Serial.println("CRC is not valid!");
       return;
   }
+
   Serial.println();
- 
+
   // the first ROM byte indicates which chip
   switch (addr[0]) {
     case 0x10:
@@ -126,17 +134,17 @@ void loop() {
     default:
       Serial.println("Device is not a DS18x20 family device.");
       return;
-  } 
+  }
 
   ds.reset();
   ds.select(addr);
   ds.write(0x44, 1);        // start conversion, with parasite power on at the end
-  
+
   delay(1000);     // maybe 750ms is enough, maybe not
   // we might do a ds.depower() here, but the reset will take care of it.
-  
+
   present = ds.reset();
-  ds.select(addr);    
+  ds.select(addr);
   ds.write(0xBE);         // Read Scratchpad
 
   Serial.print("  Data = ");
@@ -147,6 +155,7 @@ void loop() {
     Serial.print(data[i], HEX);
     Serial.print(" ");
   }
+
   Serial.print(" CRC=");
   Serial.print(OneWire::crc8(data, 8), HEX);
   Serial.println();
@@ -170,6 +179,7 @@ void loop() {
     else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
     //// default is 12 bit resolution, 750 ms conversion time
   }
+
   celsius = (float)raw / 16.0;
   fahrenheit = celsius * 1.8 + 32.0;
   Serial.print("  Temperature = ");
@@ -186,17 +196,15 @@ void loop() {
   char radiopacket[20];
   dtostrf(celsius, 5,2,radiopacket); // HERE IS THE RADIOPACKET - NEED TO ADD IN ROM, THIS JUST CONVERTS FROM FLOT TO CHAR
 
-  
   Serial.println(radiopacket);
-  
+
 //TRANSMISSION
-  
   delay(1000);  // Wait 1 second between transmits, could also 'sleep' here!
-  
+
   //char radiopacket[20] = "Temperature ";
   //itoa(celsius, radiopacket+12, 10);
   Serial.print("Sending "); Serial.println(radiopacket);
-    
+
   if (radio.sendWithRetry(RECEIVER, radiopacket, strlen(radiopacket))) { //target node Id, message as string or byte array, message length
     Serial.println("OK");
     Blink(LED, 50, 3); //blink LED 3 times, 50ms between blinks
@@ -206,6 +214,7 @@ void loop() {
   Serial.flush(); //make sure all serial data is clocked out before sleeping the MCU
 }
 
+//Blink function
 void Blink(byte PIN, byte DELAY_MS, byte loops)
 {
   for (byte i=0; i<loops; i++)
